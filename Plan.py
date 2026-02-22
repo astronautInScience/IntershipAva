d4c1b7d3cf0e4721a2c7dad7e701095fa390e188
Ghazaleh <yousefighazal3@gmail.com>
2026-02-22 12:00:02 +0100

    Add comment for testing logic errors

---
+++ Plan.py
@@ -0,0 +1,33 @@
+#pseudocode
+#define pi pins
+M1_STEP = 18
+M1_DIR  = 23
+M1_ENA  = 24
+
+M2_STEP = 5
+M2_DIR  = 6
+M2_ENA  = 13
+
+#Pi will recive a TTL signal coming from optic 
+
+#mesurment of spectumeter (taking time)
+
+
+#Pi will give motor drive a signal 
+
+
+#so it will signal the step motors to rotate(stage is moving)
+
+#stage need to move in xy axis
+
+# (input) the dimentions are given-> the surface will get divided into samll 1 to 1 mm cubes
+#moves in steps till full the whole surface
+
+#tringer out (stoping the process)
+
+# think about object and algorithm design
+
+#test for logic errors
+
+
+
